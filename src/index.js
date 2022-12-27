07136324c54452ed0645f8607509b33ab9b773d5
minacio00 <mateus_aruana@hotmail.com>
2022-12-27 13:45:02 -0300

    working routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-const express =  require('express');
-const credentials = require('./firebaseCredentials');
+const express = require('express');
+const cors =  require('cors');
+// import { firebaseConfig } from './firebaseCredentials.js';
+// import {initializeApp} from "firebase/app";
+//todo : savlar dados no backend, criar rota que devolve json do campeonato e todos os campeonatos disponíveis
 
 const app =  express();
-
-app.get('/',(req,res) => {
-    res.send("Hello world!");
-});
-app.listen(3000, () => { console.log("listening to 3000") });
+app.use(cors({credentials: true, origin: true}));
+app.options("*",cors());
+app.use(express.json());
+// const firebaseapp = initializeApp(firebaseConfig);
+app.post('/newevent', (req, res) => {
+  console.log(req.body)
+  res.status(200).json({"user": "amado"});
+}
+);
+app.listen(8080, () => { console.log("listening to 8080") });
