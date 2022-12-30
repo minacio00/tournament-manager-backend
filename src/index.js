270bcec83369264f85d2af40824016c0e4fb5d5a
minacio00 <mateus_aruana@hotmail.com>
2022-12-29 23:46:18 -0300

    returning bracket

--- src/index.js
+++ src/index.js
@@ -10,8 +10,40 @@ app.options("*",cors());
 app.use(express.json());
 // const firebaseapp = initializeApp(firebaseConfig);
 app.post('/newevent', (req, res) => {
-  console.log(req.body)
-  res.status(200).json({"user": "amado"});
+  // console.log(req.body)
+  const NumberOfParticipants  = parseInt(req.body.NumberOfParticipants);
+  const bracket = [];
+  let participantsId = 0;
+  console.log(bracket.length)
+  for (let index = 0; index < NumberOfParticipants-1; index++){
+    bracket.push({
+      id: index,
+      nextMatchId: index+1,
+      tournamentRoundText: '3',
+      startTime: '2021-05-30',
+      state: 'SCHEDULED',
+      participants: [
+        {
+          id: participantsId,
+          resultText: null,
+          stataus: 'SCHEDULED',
+          name: 'TBD',
+          picture: '',
+        }
+      ,
+      {
+        id: participantsId+1,
+        resultText: null,
+        stataus: 'SCHEDULED',
+        name: 'TBD',
+        picture: '',
+      }
+    ],
+    })
+    participantsId+2;
+  }
+  console.log(bracket);
+  res.status(200).json(bracket);
 }
 );
 app.listen(8080, () => { console.log("listening to 8080") });
