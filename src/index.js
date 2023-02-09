3bd42c103e71b7061047938a4bbf658f9ee821fe
minacio00 <mateus_aruana@hotmail.com>
2023-02-09 01:43:26 -0300

    refactoring and README edit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 const express = require('express');
 const cors =  require('cors');
+const serverRouter = require('./Routes');
 
-const {firebaseApp} = require('./firebaseCredentials');
-const tournamentController = require('./Controllers/tournamentController');
-
-// import { firebaseConfig } from './firebaseCredentials.js';
-// import {initializeApp} from "firebase/app";
-//todo : savlar dados no backend, criar rota que devolve json do campeonato e todos os campeonatos disponíveis
 
 const app =  express();
 app.use(cors({credentials: true, origin: true}));
 app.options("*",cors());
 app.use(express.json());
 
-app.get('/tournaments/view',tournamentController.tournaments);
+app.use(serverRouter);
 
-app.post('/register/:username', tournamentController.userSubscription);
-app.post('/newevent', tournamentController.newBracket);
 app.listen(8080, () => { console.log("listening to 8080") });
 module.exports = app;
